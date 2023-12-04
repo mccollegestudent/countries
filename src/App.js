98e0a33ddd14494906570c8f1bcf59d4abb930b2
Manatsa Chiomadzi <94277576+mccollegestudent@users.noreply.github.com>
2023-12-04 14:14:43 -0500

    creating a component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
 
-function App() {
+class App extends React.Component {
+  render() {
   return (
     <div className="App">
       <header className="App-header">
@@ -18,8 +20,12 @@ function App() {
           Learn React
         </a>
       </header>
+      <p>
+        To get started, edit
+      </p>
     </div>
   );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Hello from './Hello'
 import reportWebVitals from './reportWebVitals';
+import'tachyons'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+
+  <Hello greetings={"Hello" + "React Ninja"}/>
+
+
   </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
