f005ab86660c4b3ccff38085ac41a0ebc5db3bf1
Manatsa Chiomadzi <94277576+mccollegestudent@users.noreply.github.com>
2023-12-05 17:34:46 -0500

    added an onchange event for the search

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from './CardList'
 import SearchBox from './SearchBox'
 import {robots} from './robots'
 
-const state = {
-    robots: robots,
-    searchfield: ''
-}
-const App = () => {
-    return (
-        <div className='tc'>
-            <h1>Robofriends</h1>
-            <SearchBox/>
-            <CardList robots = {robots}/>
-        </div>
 
-    )
+class App extends Component {
+    constructor (){
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }    
+    }
+
+    onSearchChange(event){ //want this func to trigger everytime there is a change
+        console.log(event.target.value);
+
+    }
+
+    render(){   //since its an object you have to say this.
+        return (
+            <div className='tc'>
+                <h1>Robofriends</h1>
+                <SearchBox searchfield={this.searchfield} searchChange={this.onSearchChange}/>
+                <CardList robots = {this.state.robots}/>
+            </div>
+
+        )
+    }
 }
 
 export default App;
