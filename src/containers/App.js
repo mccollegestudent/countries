7e54efeba8d5840d8ca59dc0fe0b85b5e8637072
Manatsa Chiomadzi <94277576+mccollegestudent@users.noreply.github.com>
2023-12-08 00:10:41 -0500

    more syntactic cleaning up

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,8 +6,6 @@ import '../index.css';
 import './App.css';
 import Scroll from '../components/Scroll';
 
-
-
 class App extends Component {
     constructor (){
         super()
@@ -28,29 +26,25 @@ class App extends Component {
 
     }
 
-    render(){   //since its an object you have to say this
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    render(){   
+        const {robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
          
         })
 
-        if (this.state.robots.length === 0){
-            return <h1>Loading</h1>
-        } else {
-            return (
-                <div className='tc'>
-                    <h1 className= 'f2'>Robofriends</h1>
-                    <SearchBox searchfield={this.searchfield} searchChange={this.onSearchChange}/>
+        return (!robots.length) ?
+        <h1>Loading</h1> :
+        (
+            <div className='tc'>
+                <h1 className= 'f2'>Robofriends</h1>
+                <SearchBox searchfield={this.searchfield} searchChange={this.onSearchChange}/>
+                <Scroll>
+                    <CardList robots = {filteredRobots}/>
+                </Scroll>
 
-                    <Scroll>
-                        <CardList robots = {filteredRobots}/>
-                    </Scroll>
-
-                </div>
-    
-            )
-        }
-
+            </div>
+        )
 
     }
 }
