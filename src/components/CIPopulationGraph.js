5bee819b2b4a719a7a8a0c1319b9a681012dcef9
Manatsa Chiomadzi <94277576+mccollegestudent@users.noreply.github.com>
2024-01-07 18:16:01 -0500

    minor update

--- src/components/CIPopulationGraph.js
+++ src/components/CIPopulationGraph.js
@@ -10,7 +10,6 @@ const CIPopulationGraph = ({ population}) => {
                     width='100%'
                     height='100%'
                     data={population}
-                    // style={{width:"100%", height:'50vh', margin:'3px'}}
                     margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
                 >
                     <XAxis dataKey="year" />
